fdfc5fffda29c2ad33872ed89bc5cca9c7440823
Afelua <e.semenova@sereniry.spb.ru>
2015-02-25 19:22:51 +0300

    Все новые файлы

---
+++ mail-it.php
@@ -0,0 +1,49 @@
+<?php
+
+/* Code by David McKeown - craftedbydavid.com */
+/* Editable entries are bellow */
+
+$send_to = "e.semenova@serenity.spb.ru";
+$send_subject = "Ajax form ";
+
+
+
+/*Be careful when editing below this line */
+
+$f_name = cleanupentries($_POST["name"]);
+$f_phone = cleanupentries($_POST["phone"]);
+$from_ip = $_SERVER['REMOTE_ADDR'];
+$from_browser = $_SERVER['HTTP_USER_AGENT'];
+
+function cleanupentries($entry) {
+	$entry = trim($entry);
+	$entry = stripslashes($entry);
+	$entry = htmlspecialchars($entry);
+
+	return $entry;
+}
+
+$message = "Заявка отправлена " . date('m-d-Y') . 
+"\n\nИмя: " . $f_name . 
+"\n\nТелефон: " . $f_phone . 
+"\n\n\nTechnical Details:\n" . $from_ip . "\n" . $from_browser;
+
+$send_subject .= " - {$f_name}";
+
+$headers = "From: " . $f_phone . "\r\n" .
+    "Reply-To: " . $f_phone . "\r\n" .
+    "X-Mailer: PHP/" . phpversion();
+
+if (!$f_phone) {
+	echo "no email";
+	exit;
+}else if (!$f_name){
+	echo "no name";
+	exit;
+}else{
+	
+		mail($send_to, $send_subject, $message, $headers);
+		echo "true";
+}
+
+?>
